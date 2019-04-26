bb445f5da5add9e861e111ce4cd65f24e9169aef
Nilsonasn <Mr.andrew.nilson@gmail.com>
2019-04-26 14:14:52 -0600

    Finished binding heroes
    Finished Hero bindings now binding some radio buttons

--- OverwatchGameStats/OverwatchGameStats/Models/GameStats.cs
+++ OverwatchGameStats/OverwatchGameStats/Models/GameStats.cs
@@ -9,16 +9,25 @@ namespace OverwatchGameStats.Models
     public class SingleGameData
     {
         private Heroes heroes;
+        private bool[] communication = new bool[4];
 
         public Heroes AllHeroes
         {
             get { return heroes; }
             set { heroes = value; }
+            
+        }
+        public bool[] Communication{
+            get { return communication; }
+            set { communication = value; }
+
         }
 
         public SingleGameData()
         {
             AllHeroes = new Heroes();
+
+            
         }
 
         
