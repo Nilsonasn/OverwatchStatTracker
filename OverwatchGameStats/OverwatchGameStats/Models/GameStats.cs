246b3ba7ac1ff24a12dd9ee53e17260d1cf77c7c
Nilsonasn <Mr.andrew.nilson@gmail.com>
2019-04-27 19:38:14 -0600

    GameType binding

--- OverwatchGameStats/OverwatchGameStats/Models/GameStats.cs
+++ OverwatchGameStats/OverwatchGameStats/Models/GameStats.cs
@@ -8,10 +8,12 @@ using System.Threading.Tasks;
 namespace OverwatchGameStats.Models
 {
     public enum CommRating {Toxic, None, Average, Good }
+    public enum GameTypeEnum { Quickplay, Competitive}
     public class SingleGameData : Screen
     {
         private Heroes heroes;
         private bool[] communication = new bool[4];
+        private bool[] gametype = new bool[2];
         private DateTime gamedate = DateTime.Now;
         private int timeHr;
         private int timeMin;
@@ -27,9 +29,17 @@ namespace OverwatchGameStats.Models
 
         }
 
+        public bool[] GameType
+        {
+            get { return gametype; }
+            set { gametype = value; }
+        }
+
         public SingleGameData()
         {
             AllHeroes = new Heroes();
+            communication[1] = true;
+            gametype[1] = true;
 
         }
 
